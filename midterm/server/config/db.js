d15d6cb3fa4234c43e59f906bf1bc076c037ba8b
tungnguyen151201 <tungnguyen151201@gmail.com>
2022-11-22 22:43:15 +0700

    edit db url

--- midterm/server/config/db.js
+++ midterm/server/config/db.js
@@ -1,4 +1,6 @@
+const password = 141517;
+
 module.exports = {
-  urls: process.env.DATABASE_URL || 'mongodb://localhost:27017',
+  urls: process.env.DATABASE_URL || `mongodb+srv://huytung:${password}@cluster0.szpdftl.mongodb.net/advanced_web_2022_midterm?retryWrites=true&w=majority`,
   port: 27017,
 };
