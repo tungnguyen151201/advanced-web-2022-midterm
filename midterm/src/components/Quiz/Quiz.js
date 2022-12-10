c23a13ab3f99db5efd633617f535174698b0e639
tnghung <tnghung2812@gmail.com>
2022-12-10 20:27:25 +0700

    Add Slide component

--- midterm/src/components/Quiz/Quiz.js
+++ midterm/src/components/Quiz/Quiz.js
@@ -1,4 +1,5 @@
 import './Quiz.css';
+import Slide from '../Slide/Slide';
 import React, { useState } from 'react';
 const Quiz = (props) => {
   return (
@@ -6,10 +7,17 @@ const Quiz = (props) => {
       <div className="quiz__header">
         <button className="quiz__btn quiz__btn--b">+ New slide</button>
         <button className="quiz__btn quiz__btn--g m-r">Import PowerPoint</button>
-        <button className="quiz__btn">Save</button>
+        <button className="quiz__btn black">Save</button>
         <button className="quiz__btn quiz__btn--g">Share</button>
         <button className="quiz__btn quiz__btn--b">Present</button>
       </div>
+      <main className="quiz__content">
+        <div className="quiz__slide">
+          <Slide />
+        </div>
+        <div className="quiz__present"></div>
+        <div className="quiz__edit"></div>
+      </main>
     </div>
   );
 };
