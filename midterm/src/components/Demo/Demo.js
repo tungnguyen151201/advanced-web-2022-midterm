273668f369c99718e692e41f6ae5b5c3760cc4a6
tnghung <tnghung2812@gmail.com>
2022-12-12 14:35:48 +0700

    Make the copy button

--- midterm/src/components/Demo/Demo.js
+++ midterm/src/components/Demo/Demo.js
@@ -5,16 +5,20 @@ import './Demo.css';
 
 const Demo = () => {
   const navigate = useNavigate();
-
-  const [copyLink, setCopyLink] = useState('');
-
+  const link = 'haha/test';
+  const handleCopy = () => {
+    navigator.clipboard.writeText(link);
+    alert('Link copied to the clipboard');
+  };
   return (
     <div className="demo__container">
       <div className="demo__content">
         <p className="demo__title">Link the test</p>
         <div className="demo__link">
-          <input className="demo__input" value="haha/test" />
-          <button className="btn__copy">Copy link</button>
+          <input className="demo__input" value={link} />
+          <button className="btn__copy" onClick={handleCopy}>
+            Copy link
+          </button>
         </div>
         <h1 className="demo__question">What's your name?</h1>
         <div className="demo__chart">
