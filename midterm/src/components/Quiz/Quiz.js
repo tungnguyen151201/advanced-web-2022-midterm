f586741726f5f144c0d6f7aedb6e3534dcc7690e
TiepHoang23 <89854924+TiepHoang23@users.noreply.github.com>
2022-12-14 14:29:47 +0700

    fix back end

--- midterm/src/components/Quiz/Quiz.js
+++ midterm/src/components/Quiz/Quiz.js
@@ -7,26 +7,32 @@ import { useNavigate } from 'react-router-dom';
 
 const Quiz = (props) => {
   const navigate = useNavigate();
+  const [slide, newSlide] = useState([0]);
   const handleDemo = () => {
     navigate('/demo');
   };
+  const handleNewSlide = () => {
+    newSlide((arr) => [...arr, `${arr.length + 1}`]);
+  };
   return (
-    <div className="quiz__container">
-      <div className="quiz__header">
-        <button className="quiz__btn quiz__btn--b m-r">+ New slide</button>
-        <button className="quiz__btn black">Save</button>
-        <button className="quiz__btn quiz__btn--b" onClick={() => handleDemo()}>
+    <div className='quiz__container'>
+      <div className='quiz__header'>
+        <button className='quiz__btn quiz__btn--b m-r' onClick={handleNewSlide}>
+          + New slide
+        </button>
+        <button className='quiz__btn black'>Save</button>
+        <button className='quiz__btn quiz__btn--b' onClick={() => handleDemo()}>
           Demo
         </button>
       </div>
-      <main className="quiz__content">
-        <div className="quiz__slide">
-          <Slide />
+      <main className='quiz__content'>
+        <div className='quiz__slide'>
+          {slide.map((value) => {
+            return <Slide key={value} />;
+          })}
         </div>
-        <div className="quiz__present">
-          <Present />
-        </div>
-        <div className="quiz__edit">
+
+        <div className='quiz__edit'>
           <Edit />
         </div>
       </main>
