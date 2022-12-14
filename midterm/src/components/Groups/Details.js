5e9c0ce22a53a5bf1f17369b20c0b114ff86b2ae
tnghung <tnghung2812@gmail.com>
2022-12-14 20:43:12 +0700

    change

--- midterm/src/components/Groups/Details.js
+++ midterm/src/components/Groups/Details.js
@@ -38,34 +38,32 @@ const Groups = () => {
   console.log(owner);
   return (
     <React.Fragment>
-      <div className='group-container'>
-        <Button variant='primary' onClick={() => handleOnGetLink()}>
+      <div className="group-container">
+        <Button variant="primary" onClick={() => handleOnGetLink()}>
           Create Link
         </Button>
-        <Button variant='primary' onClick={() => handleOnSendEmail()}>
+        <Button variant="primary" onClick={() => handleOnSendEmail()}>
           Send Invite Email
         </Button>
         <h1>Owners: {owner.username}</h1>
         <h1>Co-Owners</h1>
-        <ul className='list-group'>
+        <ul className="list-group">
           {coowner.listitems.map((listitem) => (
-            <li className='list-group-item list-group-item-primary'>
+            <li className="list-group-item list-group-item-primary">
               {listitem.username}{' '}
-              <span className='group-icons'>
-                <BsFillArchiveFill className='delete-icon' />{' '}
-                <BsFillPencilFill className='edit-icon' />
+              <span className="group-icons">
+                <BsFillArchiveFill className="delete-icon" /> <BsFillPencilFill className="edit-icon" />
               </span>
             </li>
           ))}
         </ul>
         <h1>Members</h1>
-        <ul className='list-group'>
+        <ul className="list-group">
           {members.listitems.map((listitem) => (
-            <li className='list-group-item list-group-item-primary'>
+            <li className="list-group-item list-group-item-primary">
               {listitem.username}{' '}
-              <span className='group-icons'>
-                <BsFillArchiveFill className='delete-icon' />{' '}
-                <BsFillPencilFill className='edit-icon' />
+              <span className="group-icons">
+                <BsFillArchiveFill className="delete-icon" /> <BsFillPencilFill className="edit-icon" />
               </span>
             </li>
           ))}
