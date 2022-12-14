9e7e100482bef9a16cb7d05983312703d4cc6d17
TiepHoang23 <89854924+TiepHoang23@users.noreply.github.com>
2022-12-14 15:34:42 +0700

    box chat

--- midterm/src/components/Demo/Demo.js
+++ midterm/src/components/Demo/Demo.js
@@ -1,29 +1,44 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 import BarChart from '../BarChart/BarChart';
+import BoxChat from '../ChatBox/BoxChat';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
 import './Demo.css';
-
 const Demo = () => {
-  const navigate = useNavigate();
-
+  const [open, setOpen] = useState(false);
   const link = 'http://localhost:3000/voting';
   const handleCopy = () => {
     navigator.clipboard.writeText(link);
     alert('Link copied to the clipboard');
   };
   return (
-    <div className="demo__container">
-      <div className="demo__content">
-        <p className="demo__title">Link the test</p>
-        <div className="demo__link">
-          <input className="demo__input" value={link} />
-          <button className="btn__copy" onClick={handleCopy}>
+    <div className='demo__container'>
+      <div className='demo__content'>
+        <p className='demo__title'>Link the test</p>
+        <div className='demo__link'>
+          <input className='demo__input' value={link} />
+          <button className='btn__copy' onClick={handleCopy}>
             Copy link
           </button>
         </div>
-        <h1 className="demo__question">What's your name?</h1>
-        <div className="demo__chart">
+        <h1 className='demo__question'>What's your name?</h1>
+
+        <div className='demo__chart'>
           <BarChart />
         </div>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls='example-collapse-text'
+          aria-expanded={open}
+        >
+          Box Chat
+        </Button>
+        <Collapse in={open}>
+          <div id='example-collapse-text'>
+            <BoxChat></BoxChat>
+          </div>
+        </Collapse>
       </div>
     </div>
   );
--- midterm/src/components/Present/Present.js
+++ midterm/src/components/Present/Present.js
@@ -1,18 +1,35 @@
 import './Present.css';
-
+import BoxChat from '../ChatBox/BoxChat';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 export default function Present() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="present">
-        <p className="question">Question 1: What's your name?</p>
-        <div className="answers">
-          <div className="answer">Answer 1</div>
-          <div className="answer">Answer 2</div>
-          <div className="answer">Answer 3</div>
-          <div className="answer">Answer 4</div>
+      <div className='present'>
+        <p className='question'>Question 1: What's your name?</p>
+        <div className='answers'>
+          <div className='answer'>Answer 1</div>
+          <div className='answer'>Answer 2</div>
+          <div className='answer'>Answer 3</div>
+          <div className='answer'>Answer 4</div>
         </div>
+        <Button
+          onClick={() => setOpen(!open)}
+          aria-controls='example-collapse-text'
+          aria-expanded={open}
+        >
+          Box Chat
+        </Button>
+        <Collapse in={open}>
+          <div id='example-collapse-text'>
+            <BoxChat></BoxChat>
+          </div>
+        </Collapse>
       </div>
-      <button className="btn__present">Pressent notes</button>
+      <button className='btn__present'>Pressent notes</button>
     </>
   );
 }
